4ca01efdb369c5ad3fa32857682fab58d20845a7
JoseAntoHL <71193848+JoseAntoHL@users.noreply.github.com>
2024-04-06 05:10:13 +0000

    Soluciones

---
+++ Pregunta09.py/Calculos.py
@@ -0,0 +1,9 @@
+import Operaciones
+
+a = int(input("Escriba el primer numero: "))
+b = int(input("Escriba el segundo numero: "))
+
+print(f"Suma: {Operaciones.suma(a,b)}")
+print(f"Resta: {Operaciones.resta(a,b)}")
+print(f"Producto: {Operaciones.producto(a,b)}")
+print(f"División: {Operaciones.division(a,b)}")
